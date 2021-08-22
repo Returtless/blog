f36464c4f65b5bed750919590c376837e61a1afd
Vladislav Likhachev <mymindissharp@gmail.com>
2021-08-22 19:04:26 +0300

    Добавил удаление и редактирование записи из бд

--- src/main/java/ru/returtless/blog/controllers/BlogController.java
+++ src/main/java/ru/returtless/blog/controllers/BlogController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.returtless.blog.models.Post;
 import ru.returtless.blog.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -34,6 +39,49 @@ public class BlogController {
     public String blogPostAdd(@RequestParam String title, @RequestParam String announce, @RequestParam String full_text, Model model){
         Post post = new Post(title, announce, full_text);
         postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        model.addAttribute("title", "Просмотр статьи");
+        Optional<Post> post = postRepository.findById(id);
+        List<Post> posts = new ArrayList<>();
+        post.ifPresent(posts :: add);
+        model.addAttribute("post",posts);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        model.addAttribute("title", "Редактирование статьи");
+        Optional<Post> post = postRepository.findById(id);
+        List<Post> posts = new ArrayList<>();
+        post.ifPresent(posts :: add);
+        model.addAttribute("post",posts);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostAdd(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String announce, @RequestParam String full_text, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setAnnounce(announce);
+        post.setText(full_text);
+        post.setTitle(title);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostRemove(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
         return "redirect:/blog";
     }
 }
