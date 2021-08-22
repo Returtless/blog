55bdb4b4e6850132b7f14bc2c56d0c627ab14dc7
Vladislav Likhachev <mymindissharp@gmail.com>
2021-08-22 18:05:56 +0300

    Добавил запись в БД

--- src/main/java/ru/returtless/blog/controllers/BlogController.java
+++ src/main/java/ru/returtless/blog/controllers/BlogController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.returtless.blog.models.Post;
 import ru.returtless.blog.repo.PostRepository;
 
@@ -17,8 +19,21 @@ public class BlogController {
     @GetMapping("/blog")
     public String about(Model model) {
         Iterable<Post> posts = postRepository.findAll();
-        model.addAttribute("posts",posts);
+        model.addAttribute("posts", posts);
         model.addAttribute("title", "Главная страница");
         return "blog";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        model.addAttribute("title", "Добавление статьи");
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String announce, @RequestParam String full_text, Model model){
+        Post post = new Post(title, announce, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
--- src/main/java/ru/returtless/blog/models/Post.java
+++ src/main/java/ru/returtless/blog/models/Post.java
@@ -1,25 +1,34 @@
 package ru.returtless.blog.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "post")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
     private String title;
     private String announce;
     private String text;
     private int views;
 
+    public Post() {
+    }
+
+    public Post(String title, String announce, String text) {
+        this.title = title;
+        this.announce = announce;
+        this.text = text;
+    }
+
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
+
     public Long getId() {
         return id;
     }
